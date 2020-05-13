663cb2738f9010fedb0eff7e9de9ac8731338e68
jamie.hankins <jamie.hankins@envestnet.com>
2020-05-13 15:49:32 -0700

    Added payment

--- Transactions.js
+++ Transactions.js
@@ -34,6 +34,7 @@ var payments = [
 { "date": "2/1/2020", "amount": 1000 },
 { "date": "2/28/2020", "amount": 1000 },
 { "date": "4/10/2020", "amount": 1000 },
+{ "date": "5/7/2020", "amount": 1000 },
 ];
 var advances = [
     { "date": "7/27/2016", "amount": 90000 },
